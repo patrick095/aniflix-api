cd6899105c7cfe969fa9c08eb072c543e87d2abb
Patrick Chaves <patrick095@gmail.com>
2020-08-09 22:53:22 -0300

    change ports and vars for work with heroku

--- src/repositories/mongodb.js
+++ src/repositories/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/aniflix',{
+//process.env.MONGODB_URL
+mongoose.connect( process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/aniflix',{
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: false
