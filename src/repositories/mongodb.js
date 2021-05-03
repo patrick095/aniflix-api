207cf2d1ff85d457f0709988c651c4267ed18aa3
Patrick Chaves <65136866+patrick095@users.noreply.github.com>
2021-05-03 18:28:57 -0300

    Update mongodb.js

--- src/repositories/mongodb.js
+++ src/repositories/mongodb.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-//process.env.MONGODB_URL
-const Url = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/aniflix'
-mongoose.connect( '${Url}',{
+require('dotenv').config();
+mongoose.connect( '${process.env.MONGODB_URL}',{
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: false
